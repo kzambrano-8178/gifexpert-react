221284752fdb93187dbbb3d6a9b6a168c9781245
Kevin Zambrano <kvin_barsa@hotmail.com>
2020-09-18 16:04:09 -0500

    docs agregado

---
+++ docs/precache-manifest.06430b2d6f0d3751da23c45acf4cf8d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef07ae6b70056c8f7be7d97d948b016",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b428ea4c100d7730d7cb",
+    "url": "/static/css/main.d73ecb01.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b428ea4c100d7730d7cb",
+    "url": "/static/js/main.8d353087.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
---
+++ src/GifExpertApp.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+
+
+    const [categories, setCategories]= useState(['Dragon ball'])
+
+    // const handleAdd = () =>{
+    // //   setCategories([...categories,'HunterXHunter'])
+    // setCategories(cats => [ ...cats, 'HunterXHunter'])
+    // }
+
+
+    return(
+    <>
+        <h2>GifExpertApp</h2>
+        <AddCategory setCategories={setCategories}/>
+        <hr />
+
+      
+
+        <ol >
+           {
+           categories.map(category => (
+               // <li key={category}> {category}</li>
+               <GifGrid 
+               key={category}
+               category={category} />
+           ))
+           }
+        </ol>
+    </>
+    )
+}
+
+
